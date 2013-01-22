79d6d86534b4599b3ce86de96908197bcbb96414
Andrey Shipilov <a@andreyshipilov.com>
2013-01-22 20:48:48 +1030

    Midcommit.

--- wknd_project/settings.py
+++ wknd_project/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 ROOT_URLCONF = 'wknd_project.urls'
 WSGI_APPLICATION = 'wknd_project.wsgi.application'
